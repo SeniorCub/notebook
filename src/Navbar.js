d3862a3934b1b9be7ee78ef4085ee6d4d9cc3e7f
SeniorCub <farinderif@gmail.com>
2024-05-02 23:30:00 +0100

    "Remove logo, adjust App and App-header styles, and add Navbar component"
    The changes include:
    
    * Removing the import statement for the logo image
    * Modifying the App and App-header styles, including changing

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css'
+
+function Navbar ()  {
+     return ( 
+          <header className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+               <div className="brand">NoteBook</div>
+          </header>
+     );
+}
+ 
+export default Navbar;
