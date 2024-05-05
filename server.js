f77a2dd4df141fd4bd15eb48b70ee164666552e1
SeniorCub <farinderif@gmail.com>
2024-05-05 17:03:48 +0100

    "Update server.js: import using es6 syntax, add 404 error handling, and update port configuration"
    This commit updates the server.js file by:
    
    1. Using es6 syntax for importing express and

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 
-const app = express()
+const app = express();
 dotenv.config();
 
-app.use(express.json())
-app.use("/", express.static(path.join(process.cwd(), "/public")))
+app.use(express.json());
+app.use("/", express.static(path.join(process.cwd(), "/public")));
 
-const PORT = (process.env.PORT || 1111 );
-app.listen(PORT, ()=>{
-     console.log(`Server running on http://localhost:${PORT}`);
-})
+// Middleware for handling 404 errors
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(process.cwd(), "/View/404.html"));
+});
+
+const PORT = process.env.PORT || 1111;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
